8afb9c2717f928796f55600707424660810f5fe9
Jonas Wagner <veers@gmx.ch>
2007-09-03 05:17:45 +0200

    Win32 fix

--- dist.sh
+++ dist.sh
@@ -4,5 +4,5 @@ cp -R src dist/lanshark
 cp -R share dist/lanshark
 cp CHANGELOG INSTALL README HACKING LICENSE dist/lanshark
 cd dist
-tar -cjf lanshark-0.0.0.tar.bz2 lanshark/
+tar -cjf lanshark-0.0.1.tar.bz2 lanshark/
 rm -rf lanshark
