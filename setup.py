a3018f26b6fd89b86a4eb98132585f5c7fb429af
Jonas Wagner <veers@gmx.ch>
2007-09-08 15:48:28 +0200

    Modified setup process: - removed warnings - only installing simplejson if its not already installed - the version is now in src/lanshark/lib.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 #from distutils.core import setup
+import sys
+sys.path += "src"
+import lanshark.lib
 from distutils.core import setup
 import os
-try:
-    import py2exe
-except ImportError:
-    pass
 
 def ls_r(dir):
     def do_reduce(a, b):
@@ -16,31 +15,26 @@ def ls_r(dir):
         return a
     return reduce(do_reduce, os.walk(dir), [])
 
-options = []
-
-setup(name='lanshark',
-      version='0.0.1',
-      description='Filesharing tool for local area networks',
-      author='Jonas Wagner',
-      author_email='veers@gmx.ch',
-      url='http://lanshark.29a.ch',
-      packages=['lanshark', 'simplejson'],
-      package_dir={'lanshark': 'src/lanshark',
-          'simplejson': 'src/lanshark/simplejson'},
-      scripts=['src/lansharkgui', 'src/lansharkc'],
-      windows=[{'script': 'src/lansharkgui',
-          'icon_resources': [(1, 'lanshark.ico')],
-          'dest_base': 'lanshark'}],
-      options={'py2exe':{
+kwargs = {
+      'name': 'Lanshark',
+      'version': lanshark.lib.__version__,
+      'description': 'Filesharing tool for local area networks',
+      'author': 'Jonas Wagner',
+      'author_email': 'veers@gmx.ch',
+      'url': 'http://lanshark.29a.ch',
+      'packages': ['lanshark'],
+      'package_dir': {'lanshark': 'src/lanshark'},
+      'scripts': ['src/lansharkgui', 'src/lansharkc'],
+      'options': {'py2exe':{
           'packages': 'encodings',
           'includes': 'cairo, pango, pangocairo, atk, gobject',
           'dist_dir': 'dist/win32',
           'optimize': 2,
           }},
-      license='GNU GPL v3',
-      data_files=ls_r('share')+ls_r('bin'),
+      'license': 'GNU GPL v3',
+      'data_files': ls_r('share')+ls_r('bin'),
       #install_requires=["pygtk >= 2.10"],
-      classifiers=['Development Status :: 4 - Beta',
+      'classifiers': ['Development Status :: 4 - Beta',
         'Environment :: Console',
         'Environment :: No Input/Output (Daemon)',
         'Environment :: Web Environment',
@@ -54,4 +48,20 @@ setup(name='lanshark',
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Topic :: Communications :: File Sharing']
-      )
+}
+
+try:
+    import simplejson
+except ImportError:
+	packages += ["simplejson"]
+	package_dir['simplejson'] = 'src/lanshark/simplejson'
+
+try:
+    import py2exe
+    kwargs['windows'] = [{'script': 'src/lansharkgui',
+          'icon_resources': [(1, 'lanshark.ico')],
+          'dest_base': 'lanshark'}]
+except ImportError:
+    pass
+
+setup(**kwargs)
